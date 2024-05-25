02daafd91d4b22d27de29fdb269ac8a559f6180d
Shifalearner <shifasayyad032@gmail.com>
2024-05-25 08:56:02 +0530

    modified file

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Edusity-React-Projec/",
+  base: "/Edusity-React-Project/",
   plugins: [react()],
 })
